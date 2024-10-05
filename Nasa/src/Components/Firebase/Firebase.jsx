545b3006876088c1f72cc191a398cbaece0bf9f7
krishnanx <abhishekkrishnan@gmail.com>
2024-10-05 11:37:23 +0530

    user check

--- Nasa/src/Components/Firebase/Firebase.jsx
+++ Nasa/src/Components/Firebase/Firebase.jsx
@@ -2,13 +2,7 @@ import React from 'react'
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-    // Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  
 
 const apiKey = `${import.meta.env.VITE_APP_API_KEY}`
 const authDomain = `${import.meta.env.VITE_APP_AUTH_DOMAIN}`
