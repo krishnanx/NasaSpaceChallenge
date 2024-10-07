06f89c5f4e8de42d4a04e25ac880ec9533589959
codewizard-2004 <amalv2004@gmail.com>
2024-10-07 21:31:58 +0530

    host change

--- Nasa/vite.config.js
+++ Nasa/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {                          // Match any request starting with /api
-        target: 'http://127.0.0.1:5000', // Your Flask backend
+        target: 'https://nasaspacechallenge-pzje.onrender.com', // Your Flask backend
         changeOrigin: true,
         secure: false,                   // If using HTTPS on Flask, set to true
         rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix
